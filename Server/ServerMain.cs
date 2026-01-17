1594c2934b853d0add93229877d381556755f6d0
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-16 16:10:29 -0800

    reformated the main code (server context)

--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Threading.Tasks;
 using CitizenFX.Core;
 
-namespace lenix_sdk.Server
-{
-    public class ServerMain : BaseScript
-    {
-        public ServerMain()
-        {
-            Debug.WriteLine("Hi from lenix_sdk.Server!");
-        }
+namespace lenix_sdk.Server {
+	public class ServerMain : BaseScript {
+		public ServerMain() {
+			Debug.WriteLine("Hi from lenix_sdk.Server!");
+		}
 
-        [Command("hello_server")]
-        public void HelloServer()
-        {
-            Debug.WriteLine("Sure, hello.");
-        }
-    }
+		[Command("hello_server")]
+		public void HelloServer() {
+			Debug.WriteLine("Sure, hello.");
+		}
+	}
 }
