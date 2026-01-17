e0163f26b9175b1c1d421a5d85cd7a1889f0ef6f
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-16 16:09:05 -0800

    reformated the main code

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -1,23 +1,18 @@
-using System;
 using System.Threading.Tasks;
 using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
 
-namespace lenix_sdk.Client
-{
-    public class ClientMain : BaseScript
-    {
-        public ClientMain()
-        {
-            Debug.WriteLine("Hi from lenix_sdk.Client!");
-        }
-
-        [Tick]
-        public Task OnTick()
-        {
-            DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
-
-            return Task.FromResult(0);
-        }
+namespace lenix_sdk.Client {
+  public class ClientMain : BaseScript {
+    public ClientMain() {
+      Debug.WriteLine("Hi from lenix_sdk.Client!");
     }
+
+    [Tick]
+    public Task OnTick() {
+      DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
+
+      return Task.FromResult(0);
+    }
+  }
 }
