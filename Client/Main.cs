d34ea1bea6f6ab1810a33c07dd47eb0cba7af6a3
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-28 02:57:11 +0100

    feat(commands): add bind command support and improve sdk handler
    - Update sdk command to log "sdk typed" instead of generic message
    - Add BindARGS class to encapsulate input and command parameters
    - Register "bind" Nui callback to execute bind command with input and command
    - Register "disconnect" Nui callback to execute disconnect command and signal success

--- Client/Main.cs
+++ Client/Main.cs
@@ -6,12 +6,23 @@ using static CitizenFX.Core.Native.API;
 
 public class Main : BaseScript
 {
+  private class BindARGS
+  {
+    internal string Input { get; set; }
+    internal string Command { get; set; }
+  }
   public Main()
   {
-    Modules.DefineCommand("sdk", () => Debug.WriteLine("Hello"));
-    RegisterNuiCallback("disconnect", new Action(() =>
+    Modules.DefineCommand("sdk", () => Debug.WriteLine("sdk typed"));
+    RegisterNuiCallback("disconnect", new Action<dynamic/* null */, Action<bool>>((data, callback) =>
     {
       ExecuteCommand("disconnect");
+      callback(true);
+    }));
+    RegisterNuiCallback("bind", new Action<BindARGS, Action<bool>>((data, callback) =>
+    {
+      ExecuteCommand($"bind {data.Input} {data.Command}");
+      callback(true);
     }));
   }
 }
