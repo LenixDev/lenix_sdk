e837e8e7aaa04eb108ecb3f4d81d080cf72d5baa
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-31 00:07:00 +0100

    refactor(main): simplify command execution and remove deprecated callbacks
    - Remove Key property from BindARGS class
    - Eliminate ResourceBindARGS subclass and related callback
    - Replace multiple callbacks with a unified "execute" callback to run commands directly
    - Mark "getConfig" callback as deprecated and update to use Config.config array
    - Clean up callback implementations for clarity and brevity

--- Client/Main.cs
+++ Client/Main.cs
@@ -7,34 +7,20 @@ public class Main : BaseScript
 {
   private class BindARGS
   {
-    internal string Key { get; set; }
     internal string Command { get; set; }
-  }
-  private class ResourceBindARGS : BindARGS
-  {
-    internal string Resource { get; set; }
   }
   public Main()
   {
     DefineCommand("sdk", () => Debug.WriteLine("sdk typed"));
-    RegisterNuiCallback("disconnect", new Action<dynamic/* null */, Action<bool>>((data, callback) =>
+    RegisterNuiCallback("execute", new Action<BindARGS, Action<bool>>((data, callback) =>
     {
-      ExecuteCommand("disconnect");
+      ExecuteCommand($"{data.Command}");
       callback(true);
     }));
-    RegisterNuiCallback("bind", new Action<BindARGS, Action<bool>>((data, callback) =>
-    {
-      ExecuteCommand($"bind {data.Key} {data.Command}");
-      callback(true);
-    }));
-    RegisterNuiCallback("resourcebind", new Action<ResourceBindARGS, Action<bool>>((data, callback) =>
-    {
-      ExecuteCommand($"rbind {data.Resource} {data.Key} {data.Command}");
-      callback(true);
-    }));
+    //@deprecated
     RegisterNuiCallback("getConfig", new Action<dynamic/* null */, Action<Config[]>>((data, callback) =>
     {
-      Config[] config = Config.Items;
+      Config[] config = Config.config;
       callback(config);
     }));
   }
